5e68e61a9e56ac7d39ad343a22c9759e062fdf4b
J. Ky Marsh <jkymarsh@gmail.com>
2014-11-26 21:52:24 -0800

    add fillmore scraping

--- app.rb
+++ app.rb
@@ -8,10 +8,9 @@ get '/' do
   erb :index
 end
 
-get '/test' do
+get '/slims' do
     shows = []
 
-    # doc = Nokogiri::HTML(open("http://www.google.com/"))
     doc = Nokogiri::HTML(open("http://www.slimspresents.com/?sort=#{params[:month]}")) do |config|
         config.strict.noent
         config.strict.noerror
@@ -26,6 +25,42 @@ get '/test' do
         end
 
         date = div.at_css('.date').text.strip[2..-3].split[2].to_i
+
+        show_info = {
+            :title => title,
+            :date => date
+        }
+
+        shows << show_info
+
+        p '---------------------------------------------------------'
+        p show_info
+    end
+
+    content_type :json
+    shows.to_json
+end
+
+get '/fillmore' do
+    month = params[:month].capitalize
+    shows = []
+
+    p month
+
+    doc = Nokogiri::HTML(open("http://thefillmore.com/calendar/?m=#{month}")) do |config|
+        config.strict.noent
+        config.strict.noerror
+        config.strict.nonet
+    end
+
+    doc.at_css("div.event_data_main").css("div.calendar").each do |div|
+        title = div.at_css('.title').child.text.strip
+
+        date_block = div.at_css('.content').child.text.strip
+        month_marker = month
+        year_marker = "2014"
+
+        date = date_block[/#{month_marker}(.*?)#{year_marker}/m, 1].strip[0..-2].to_i
 
         show_info = {
             :title => title,
--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,41 @@ $('#find-slims').on('click', function(evt) {
     evt.preventDefault();
 
     $.ajax({
-        url: '/test',
+        url: '/slims',
         type: 'get',
         data: {
             month: selectedMonth
         }
     }).done(function(data) {
         $('#show-list').empty();
+
+        $('#venue-name').text('Slim\'s');
+
+        $.each(data, function(idx, element) {
+            $('#show-list').append('<li>' + element.title + ' @ ' + element.date + '</li>');
+        });
+    }).fail(function() {
+        console.log('fail');
+    });
+});
+
+$('#find-fillmore').on('click', function(evt) {
+    var selectedMonth = $('#select-month').val();
+
+    console.log(selectedMonth);
+
+    evt.preventDefault();
+
+    $.ajax({
+        url: '/fillmore',
+        type: 'get',
+        data: {
+            month: selectedMonth
+        }
+    }).done(function(data) {
+        $('#show-list').empty();
+
+        $('#venue-name').text('Fillmore');
 
         $.each(data, function(idx, element) {
             $('#show-list').append('<li>' + element.title + ' @ ' + element.date + '</li>');
